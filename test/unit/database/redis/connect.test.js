c381d5e5018f26f050d4596ee73026b83a44a7a4
hasimy-as <ahmadhasyimi674@gmail.com>
2021-04-03 07:34:32 +0700

    test: update redis test

--- test/unit/database/redis/connect.test.js
+++ test/unit/database/redis/connect.test.js
@@ -4,15 +4,9 @@ const redis = require('redis');
 const config = require('../../../../app/config/config');
 const connectRedis = require('../../../../app/database/redis/connect');
 
-const redisUrl = config.get('/');
-
 describe('Redis server', () => {
   it('should create redis pool connection', async () => {
-    sinon.stub(redis, 'createClient').resolves({
-      port: redisUrl.redis.port,
-      host: redisUrl.redis.host,
-      password: redisUrl.redis.password
-    });
+    sinon.stub(redis, 'createClient').resolves(config.get('/redis'));
     connectRedis.myPool();
     redis.createClient.restore();
   });
